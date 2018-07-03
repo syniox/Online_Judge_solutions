6b42c96331423d8390ba003ab08e83c5dc6f8db1
syniox <30317541+syniox@users.noreply.github.com>
2018-07-03 11:07:54 +0800

    [AC*]BZOJ4518 37000KB 89ms
    很多地方都没过脑子。。
    毕竟还是太naive了
    改了很多（虽然只有53行），但都忘记commit了。。

--- BZOJ/4518.cpp
+++ BZOJ/4518.cpp
@@ -2,16 +2,14 @@
 #include<cstdio>
 #include<cstring>
 using namespace std;
-const int N=3002;
-const int Q=9000005;
-int n,m,qh,qt,pre[N],dp[N][N];
-//dp[i][j]=min(dp[i-1][k]+(pre[j]-pre[k+1])^2)(i<=k<j)
-struct node{
-	int x,y;
-	node operator - (const node b) const {
-		return (node){x-b.x,y-b.y};
-	}
-}que[Q];
+typedef long long lint;
+const int N=3005;
+int n,m,qh,qt,pr[N],dp[N],fp[N],que[N];
+
+inline lint _x(int x){return pr[x]<<1;}
+
+inline lint _y(int x){return fp[x]+pr[x]*pr[x];}
+
 inline int nxi(){
 	int x=0;
 	char c;
@@ -20,43 +18,36 @@ inline int nxi(){
 	return x;
 }
 
-inline int crs(node x,node y,node z){
-	node a=y-x,b=z-y;
-	return (a.x-b.y)*(a.y-b.x);
+inline int top(int x){
+	int a,b;
+	while(qt-qh>1&&_y(b=que[qh+1])-_y(a=que[qh])<pr[x]*(_x(b)-_x(a))) ++qh;
+	return que[qh];
 }
 
-inline int q_sz(){
-	return qt-qh+(qt<qh?Q:0)+1;
+inline bool jdg(int a,int b,int c){
+	int x=_x(b),y=_y(b);
+	return (x-_x(a))*(_y(c)-y)-(_x(c)-x)*(y-_y(a))>=0;//
 }
 
-inline void q_psh(node x){
-	if(q_sz()>1){
-		node y=que[qt-2+(qt-2<0?Q:0)],z=que[qt-1==-1?Q-1:qt-1];
-		while(q_sz()>1&&crs(y,z,x)<0){
-			z=y;
-			y=que[--qt==-1?qt=Q-1:qt];
-		}
-	}
+inline void psh(int x){
+	while(qt-qh>1&&jdg(que[qt-2],que[qt-1],x)==0) --qt;
 	que[qt++]=x;
-}
-
-inline node q_top(int x){
-	node t=que[hd];
-	while(q_sz()&&t.y<t.x*x) t=que[hd++];
 }
 
 int main(){
 	n=nxi(),m=nxi();
-	for(int i=1;i<=n;++i){
-		pre[i]=pre[i-1]+nxi();
-	}
-	for(int i=1;i<=n;++i){
-		dp[1][i]=pre[i]*pre[i];
-		q_psh((node){pre[i]<<1,dp[1][i]<<1});
-	}
+	for(int i=1;i<=n;++i) pr[i]=pr[i-1]+nxi();
+	for(int i=1;i<=n;++i) fp[i]=pr[i]*pr[i];
 	for(int i=2;i<=m;++i){
-		for(int j=1;j<=n;++j){
-			node k=q_top(pre[j]);
+		//get the queue before
+		que[qh=(qt=1)-1]=i-1;
+		for(int j=i;j<=n;++j){
+			int k=top(j);
+			dp[j]=fp[k]+(pr[j]-pr[k])*(pr[j]-pr[k]);
+			psh(j);
 		}
+		memcpy(fp,dp,sizeof(dp));
 	}
+	printf("%d\n",m*dp[n]-pr[n]*pr[n]);
+	return 0;
 }
