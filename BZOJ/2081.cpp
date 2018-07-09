32c8ad0eb2da69de47b857b1340ff4271618d9b1
syniox <30317541+syniox@users.noreply.github.com>
2018-07-09 08:10:02 +0800

    [WA]BZOJ2081 97pts
    前面TLE是复杂度错误。
    不分析的后果。

--- BZOJ/2081.cpp
+++ BZOJ/2081.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 #include<cstdio>
 #include<cstring>
+#include<map>
 using namespace std;
 typedef long long lint;
 const int N=200005;
 const int ha=19260817;
 const int mod=1e9+7;
-int n,ans,tl,tk,xl[N],vc[N],zk[N],fk[N],que[N];
+int n,ans,tl,tk,xl[N],vc[N],que[N];
 lint hx[N],xh[N],qh[N];
+map <int,int> mp;
 
 inline int nxi(){
 	int x(0);
@@ -15,14 +17,6 @@ inline int nxi(){
 	while((c=getchar())>'9'||c<'0');
 	while(x=x*10+c-48,(c=getchar())>='0'&&c<='9');
 	return x;
-}
-
-lint qmi(int x,int t){
-	if(!t) return 1;
-	lint q=qmi(x,t>>1);
-	q=q*q%mod;
-	if(t&1) q=q*x%mod;
-	return q;
 }
 
 inline int zx(int x,int k){
@@ -33,23 +27,13 @@ inline int fx(int x,int k){
 	return ((xh[x]-xh[x+k]*qh[k])%mod+mod)%mod;
 }
 
-bool eql(int a,int b,int k){
-	return (zk[a]==zk[b]&&fk[a]==fk[b])||(zk[a]==fk[b]&&zk[b]==fk[a]);
-}
-
-bool jdg(int i,int k){
-	for(int j=1;j<=tk;++j){
-		if(eql(i,que[j],k)) return 0;
-	}
-	return 1;
-}
-
 inline int cal(int k){
-	int q=tk=0;
+	int q=0;
+	mp.erase(mp.begin(),mp.end());
 	for(int i=1;i<=n-k+1;i+=k){
-		zk[i]=zx(i,k),fk[i]=fx(i,k);
-		if(jdg(i,k)){
-			que[++tk]=i;
+		int p=min(zx(i,k),fx(i,k));
+		if(!mp[p]){
+			mp[p]=1;
 			++q;
 		}
 	}
