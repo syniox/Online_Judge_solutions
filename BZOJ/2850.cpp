36b73e06fe29cc073d70d8e0f95f6b04ba396aa1
syniox <30317541+syniox@users.noreply.github.com>
2018-07-11 14:07:59 +0800

    [TLE]BZOJ2850 真·复杂度错误
    调出问题可以跟我说一下吗？

--- BZOJ/2850.cpp
+++ BZOJ/2850.cpp
@@ -2,10 +2,12 @@
 #include<cstdio>
 #include<algorithm>
 #include<cstring>
+#include<ctime>
 using namespace std;
 typedef long long lint;
 const int N=5e4+5;
 int n,m,rt,ka,kb,kc;
+lint ans;
 struct node{
 	int x,p[2],c[2],mn[2],mx[2];
 	lint s;
@@ -17,77 +19,104 @@ inline bool cpy(const node &a,const node &b){
 inline bool cpx(const node &a,const node &b){
 	return a.p[0]<b.p[0];
 }
-inline void apn(int &a,const int b){
+inline void apn(int &a,const int b,const int c){
 	if(a>b) a=b;
+	if(a>c) a=c;
 }
-inline void apx(int &a,const int b){
+inline void apx(int &a,const int b,const int c){
 	if(a<b) a=b;
+	if(a<c) a=c;
 }
 
 inline int nxi(){
-	int x=0;
+	int x=0,f=1;
 	char c;
-	while((c=getchar())>'9'||c<'0');
+	while((c=getchar())>'9'||c<'0') if(c=='-') f=-f;
 	while(x=x*10+c-48,(c=getchar())>='0'&&c<='9');
-	return x;
+	return x*f;
+}
+
+inline void prt(lint x){
+	if(x>9) prt(x/10);
+	putchar(x%10+48);
 }
 
 inline void upd(int k){
 	node &p=tr[k],&l=tr[p.c[0]],&r=tr[p.c[1]];
-	for(int i=0;i<2;++i){
-		apn(p.mn[i],l.mn[i]);
-		apn(p.mn[i],r.mn[i]);
-		apx(p.mx[i],l.mx[i]);
-		apx(p.mx[i],r.mx[i]);
-	}
+	apn(p.mn[0],l.mn[0],r.mn[0]);
+	apn(p.mn[1],l.mn[1],r.mn[1]);
+	apx(p.mx[0],l.mx[0],r.mx[0]);
+	apx(p.mx[1],l.mx[1],r.mx[1]);
 	p.s+=l.s+r.s;
 }
 
-int vld(node &x){
-	int ans=0;
-	ans+=(ka*x.mn[0]+kb*x.mn[1]<kc);
-	ans+=(ka*x.mn[0]+kb*x.mx[1]<kc);
-	ans+=(ka*x.mx[0]+kb*x.mn[1]<kc);
-	ans+=(ka*x.mx[0]+kb*x.mx[1]<kc);
+inline int jdg(int x,int y){
+	return (lint)ka*x+(lint)kb*y<kc;
+}
+
+inline int vld(node &x){
+	int ans(0);
+	ans+=jdg(x.mn[0],x.mn[1]);
+	ans+=jdg(x.mn[0],x.mx[1]);
+	ans+=jdg(x.mx[0],x.mn[1]);
+	ans+=jdg(x.mx[0],x.mx[1]);
 	return ans;
 }
 
 int build(int l,int r,bool f){
-	if(l>r) return 0;
 	int mid=(l+r)>>1;
-	nth_element(tr+l,tr+mid,tr+r,f?cpy:cpx);
-	tr[mid].c[0]=build(l,mid-1,f^1);
-	tr[mid].c[1]=build(mid+1,r,f^1);
+	nth_element(tr+l,tr+mid,tr+r+1,f?cpy:cpx);
+	if(l<mid) tr[mid].c[0]=build(l,mid-1,f^1);
+	if(r>mid) tr[mid].c[1]=build(mid+1,r,f^1);
 	upd(mid);
 	return mid;
 }
 
-lint ask(int k){
-	if(!k) return 0;
-	node &p=tr[k];
-	int q=vld(p);
-	if(!q) return 0;
-	if(q==4) return p.s;
-	return (ka*p.p[0]+kb*p.p[1]<kc)*p.x+ask(p.c[0])+ask(p.c[1]);
+int ndcnt=0;
+void ask(int k){
+	++ndcnt;
+	if(jdg(tr[k].p[0],tr[k].p[1])) ans+=tr[k].x;
+	int l=tr[k].c[0],r=tr[k].c[1];
+	if(l){
+		int x=vld(tr[l]);
+		if(x==4) ans+=tr[l].s;
+		else if(x) ask(l);
+	}
+	if(r){
+		int x=vld(tr[r]);
+		if(x==4) ans+=tr[r].s;
+		else if(x) ask(r);
+	}
 }
 
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("a.in","r",stdin);
+	freopen("a.out","w",stdout);
 #endif
 	tr[0].mn[0]=tr[0].mn[1]=1e9;
+//	int t=clock();
 	n=nxi(),m=nxi();
 	for(int i=1;i<=n;++i){
 		node &p=tr[i];
-		scanf("%d%d",&p.p[0],&p.p[1]);
-		p.mn[0]=p.mx[0]=p.p[0];
-		p.mn[1]=p.mx[1]=p.p[1];
+		p.mn[0]=p.mx[0]=p.p[0]=nxi();
+		p.mn[1]=p.mx[1]=p.p[1]=nxi();
 		p.x=p.s=nxi();
 	}
 	rt=build(1,n,0);
-	while(m--){
-		scanf("%d%d%d",&ka,&kb,&kc);
-		printf("%lld\n",ask(rt));
+//	printf("***%d\n",clock()-t);
+	for(int i=1;i<=n;++i){
+		node &p=tr[i];
+		printf("l%d r%d %d %d %d %d %d %d\n",p.c[0],p.c[1],p.p[0],p.p[1],p.mn[0],p.mn[1],p.mx[0],p.mx[1]);
 	}
+	while(m--){
+		//      scanf("%d%d%d",&ka,&kb,&kc);
+		ka=nxi(),kb=nxi(),kc=nxi();
+		ans=0;
+		ask(rt);
+		prt(ans);
+		putchar('\n');
+	}
+	cerr<<"ndcnt="<<ndcnt<<endl;
 	return 0;
 }
