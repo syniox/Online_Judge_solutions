cc5c413bd8a1d4290b55717eeda4cca1a42760a2
syniox <30317541+syniox@users.noreply.github.com>
2018-07-18 16:18:50 +0800

    [RE]BZOJ4689

---
+++ BZOJ/4689.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+using namespace std;
+int dg;
+double og[8],ans[6],xx[8][6];
+//[base][time]
+inline int nxi(){
+	int x=0;
+	char c;
+	while((c=getchar())>'9'||c<'0');
+	while(x=x*10+c-48,(c=getchar())>='0'&&c<='9');
+	return x;
+}
+
+inline void init(){
+	for(int i=1;i<8;++i) xx[i][0]=1;
+	for(int i=1;i<8;++i){
+		for(int j=1;j<8;++j){
+			xx[i][j]=xx[i][j-1]*i;
+		}
+	}
+}
+
+inline void gauss(int p1,int p2){
+	static double mg[8],xs[8][6];
+	static int get[6];
+	static bool vis[8];
+	memcpy(xs,xx,sizeof(xs));
+	memcpy(mg,og,sizeof(mg));
+	memset(vis,0,sizeof(vis));
+	vis[p1]=vis[p2]=1;
+
+	for(int i=dg,j=1;i;--i,++j){
+		while(vis[j]) ++j;
+		vis[j]=1;
+		get[i]=j;
+		double p=xs[j][i];
+		for(int k=0;k<dg+3;++k){
+			if(!vis[k]) continue;
+			double t=xs[k][i]/p;
+			mg[k]-=mg[j]*t;
+			for(int l=0;l<=dg;++l){
+				xs[k][l]-=xs[j][l]*t;
+			}
+		}
+	}
+
+	get[0]=!p1||!p2?0:get[1]+1;
+	while(vis[get[0]]) ++get[0];
+	ans[0]=mg[get[0]];
+	for(int i=1;i<=dg;++i){
+		double tp(0);
+		for(int j=0;j<i;++j){
+			tp+=xs[get[i]][j]*ans[j];
+		}
+		ans[i]=(mg[get[i]]-tp)/xs[get[i]][i];
+	}
+}
+
+inline bool jdg(int k){
+	double tp=0;
+	for(int i=dg;~i;--i){
+		tp+=ans[i]*xx[k][i];
+	}
+	return fabs(tp-og[k])<1e-2;
+}
+
+
+inline int solve(){
+	for(int i=0;i<dg+3;++i){
+		for(int j=i+1;j<dg+3;++j){
+			gauss(i,j);
+			if(jdg(i)) return j;
+			if(jdg(j)) return i;
+		}
+	}
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+	freopen("a.in","r",stdin);
+#endif
+	init();
+	while(dg=nxi()){
+		for(int i=0;i<dg+3;++i) og[i]=nxi();
+		printf("%d\n",solve());
+	}
+	return 0;
+}
