aa2a838a8ad1eedb047a66858197a9f5b590ada7
syniox <30317541+syniox@users.noreply.github.com>
2018-07-20 19:35:00 +0800

    [MLE]BZOJ2599 6pts

--- BZOJ/2599.cpp
+++ BZOJ/2599.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstdio>
 #include<cstring>
+#include<cassert>
 #include<map>
 using namespace std;
 const int N=2e5+1;
@@ -18,6 +19,7 @@ template <class T> inline void apx(T &a,T b){
 }
 
 inline void add(int a,int b,int v){
+//	printf("add: %d %d %d\n",a,b,v);
 	eg[++cnt]=(edge){b,v,fir[a]};
 	fir[a]=cnt;
 }
@@ -32,7 +34,7 @@ inline int nxi(){
 
 void get_rt(int x,int fa){
 	sz[x]=1;
-	sx[x]=1e7;
+	sx[x]=0;
 	for(int i=fir[x];i;i=eg[i].nx){
 		int y=eg[i].to;
 		if(y!=fa&&!vis[y]){
@@ -47,24 +49,29 @@ void get_rt(int x,int fa){
 
 void dfs(int x,int fa){
 	mp[x].clear();
+	sz[x]=1;
 	for(int i=fir[x];i;i=eg[i].nx){
 		int y=eg[i].to,v=eg[i].wi;
 		if(!vis[y]&&y!=fa){
-			apn(mp[x][v],1);
+			if(!mp[x][v]||mp[x][v]>1){
+				mp[x][v]=1;
+			}
 			dfs(y,x);
+			sz[x]+=sz[y];
 			map<int,int>::iterator it;
 			for(it=mp[y].begin();it!=mp[y].end();++it){
-				apn(mp[x][it->first+v],it->second+1);
+				int &p=mp[x][it->first+v];
+				if(!p||p<it->second+1) p=it->second+1;
 			}
 		}
 	}
 }
 
 void work(int x){
-	dfs(x,0);
+	dfs(x,-1);
 	vis[x]=1;
 	map<int,int>::iterator it;
-	for(it=mp[x].begin();it!=mp[x].end();++it){
+	for(it=mp[x].begin();it!=mp[x].end();it++){
 		if(it->first<len){
 			if(mp[x][len-(it->first)]){
 				apn(ans,it->second+mp[x][len-(it->first)]);
@@ -78,6 +85,7 @@ void work(int x){
 		int y=eg[i].to;
 		if(!vis[y]){
 			sum=sz[y];
+			rt=y;
 			get_rt(y,x);
 			work(rt);
 		}
@@ -88,7 +96,7 @@ int main(){
 #ifndef ONLINE_JUDGE
 	freopen("b.in","r",stdin);
 #endif
-	sx[0]=1e8;
+	ans=1e9;
 	n=nxi(),len=nxi();
 	for(int a,b,c,i=1;i<n;++i){
 		a=nxi(),b=nxi(),c=nxi();
@@ -96,8 +104,8 @@ int main(){
 		add(b,a,c);
 	}
 	sum=n;
-	get_rt(1,0);
+	get_rt(1,-1);
 	work(rt);
-	printf("%d\n",ans);
+	printf("%d\n",ans==1e9?-1:ans);
 	return 0;
 }
