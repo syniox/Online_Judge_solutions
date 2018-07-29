342a9186b57ad91b9d83ef1f1d675e5d6ca58259
syniox <30317541+syniox@users.noreply.github.com>
2018-07-29 11:09:52 +0800

    [AC*]BZOJ4591 44216 KB 236 ms		lucas

--- BZOJ/4591.cpp
+++ BZOJ/4591.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 #include<cstdio>
 #include<cstring>
+typedef long long lint;
 const int mod=2333;
 int C[mod][mod],sc[mod][mod];
 
-inline int nxi(){
-	int x=0;
+template<class T> inline T nxi(){
+	T x=0;
 	char c;
 	while((c=getchar())>'9'||c<'0');
 	while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
@@ -17,7 +18,7 @@ inline void twk(int &x){
 }
 
 inline void init(){
-	C[0][0]=1;
+	C[0][0]=1,sc[0][0]=1;;
 	for(int i=0;i<mod;++i) sc[0][i]=1;
 	for(int i=1;i<mod;++i){
 		sc[i][0]=C[i][0]=1;
@@ -28,20 +29,28 @@ inline void init(){
 	}
 }
 
-int sol(int n,int k){
-#define P mod
+int lucas(const lint n,const lint k){
+	if(k<0||n<k) return 0;
+	if(n<mod) return C[n][k];
+	return lucas(n/mod,k/mod)*lucas(n%mod,k%mod)%mod;
+}
+
+int sol(const lint n,const lint k){
+	const int P=mod;
+	if(k<0) return 0;
 	if(n<P&&k<P) return sc[n][k];
-	return (sol(n/P,k/P-1)*sc[n%P][P-1]+sc[n/P][k/P-1]*sc[n%P][k%P])%P;
+	return (sol(n/P,k/P-1)*sol(n%P,n%P)+lucas(n/P,k/P)*sc[n%P][k%P])%P;
 }
 
 int main(){
 #ifndef ONLINE_JUDGE
-	freopen("4591.in","r",stdin);
+	freopen("1.in","r",stdin);
+	freopen("4591.out","w",stdout);
 #endif
 	init();
-	int T=nxi();
+	int T=nxi<int>();
 	while(T--){
-		int n=nxi(),k=nxi();
+		lint n=nxi<lint>(),k=nxi<lint>();
 		printf("%d\n",sol(n,k));
 	}
 	return 0;
