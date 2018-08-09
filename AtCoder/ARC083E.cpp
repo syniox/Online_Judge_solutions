d9dcafdc0645530a2f347e1455a563a1c744d504
syniox <30317541+syniox@users.noreply.github.com>
2018-08-09 19:36:11 +0800

    [AC*]ARC083E 	21296 KB26 ms		树形DP

--- AtCoder/ARC083E.cpp
+++ AtCoder/ARC083E.cpp
@@ -3,6 +3,10 @@
 #include<cstring>
 const int N=1002;
 
+inline void apn(int &a,int b){
+	if(a>b)a=b;
+}
+
 inline char get_c(){
 	static char buf[20000],*h,*t;
 	if(h==t){
@@ -22,22 +26,22 @@ inline int nxi(){
 int main(){
 	static int fa[N],hx[N],dp[N][5002];
 	int n=nxi();
-	for(int i=2;i<=n;++i) fa[x]=nxi();
-	for(int i=1;i<=n;++i) hx[x]=nxi();
+	for(int i=2;i<=n;++i) fa[i]=nxi();
+	for(int i=1;i<=n;++i) hx[i]=nxi();
 
 	for(int x=n;x;--x){
 		int y=fa[x];
-		for(int i=hx[y];i;--i){
+		for(int i=hx[y];i>=0;--i){
 			int p=dp[y][i];
 			dp[y][i]=1e5;
 			if(hx[x]<=i){
-				dp[y][i]=(hx[x]?p:dp[y][i-hx[x]])+dp[x][hx[x]];
+				dp[y][i]=(hx[x]?dp[y][i-hx[x]]:p)+dp[x][hx[x]];
 			}
 			if(dp[x][hx[x]]<=i){
-				dp[y][i];
+				apn(dp[y][i],(dp[x][hx[x]]?dp[y][i-dp[x][hx[x]]]:p)+hx[x]);
 			}
 		}
 	}
-	puts(dp[1][hx[1]]==1e5?"IMPOSSIBLE":"POSSIBLE");
+	puts(dp[1][hx[1]]>=1e5?"IMPOSSIBLE":"POSSIBLE");
 	return 0;
 }
