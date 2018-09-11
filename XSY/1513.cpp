666baf3899c540965a7883eff3c8b8998775a0df
syniox <30317541+syniox@users.noreply.github.com>
2018-09-11 19:15:35 +0800

    [AC] XSY1513

--- XSY/1513.cpp
+++ XSY/1513.cpp
@@ -2,7 +2,7 @@
 #include<cstdio>
 #include<cstring>
 const int N=1e5+2,mod=10007;
-int l,st,dp[N][3];
+int l,st,dp[N][3][2];
 char ch[N];
 bool used,ispr[10];
 
@@ -16,33 +16,37 @@ inline void twk(int &x){
 
 //d: top? p: all prime? p0: pre-0?
 int dfs(int t,int s,bool d,bool p,bool p0){
-	if(t==l) return s==st&&!p0;
-	if(!d&&!p&&!p0&&dp[t][s]) return dp[t][s];
+	if(t==l) return s==st&&!p&&!p0;
+	if(!d&&!p0&&dp[t][s][p]) return dp[t][s][p];
 	const int limit=d?ch[t+1]:9;
 	int ans=0;
 	if(0<limit){
-		twk(ans+=dfs(t+1,s,0,p0||p,p0));
+		twk(ans+=dfs(t+1,s,0,p0,p0));//
 	}
 	for(int i=1;i<limit;++i){
 		twk(ans+=dfs(t+1,(s+i)%3,0,p&&ispr[i],0));
 	}
-	twk(ans+=dfs(t+1,(s+limit)%3,d,p&&(ispr[limit]||(!limit&&p0)),p0&&!limit));
-	if(!d&&!p&&!p0) dp[t][s]=ans;
+	const bool f0=!limit&&p0;
+	twk(ans+=dfs(t+1,(s+limit)%3,d,(p&&ispr[limit])||f0,f0));
+//	printf("%d %d %d %d %d: %d\n",t,s,d,p,p0,ans);
+	if(!d&&!p0) dp[t][s][p]=ans;
 	return ans;
 }
 
 inline int solve(){
+	memset(dp,0,sizeof(dp));
 	scanf("%s",ch+1);
 	l=strlen(ch+1);
-	if(st) used=1;
 	for(int i=1;i<=l;++i){
 		ch[i]-='0';
 		if(!used) st+=ch[i];
 	}
-	st%=3;
-	int ans=dfs(0,0,1,1,1);
-	printf("%d\n",ans);
-	return ans;
+	if(!used) st%=3;
+	if(!used) used=1;
+//	const int ans=dfs(0,0,1,1,1);
+//	printf("%d\n",ans);
+//	return ans;
+	return dfs(0,0,1,1,1);
 }
 
 int main(){
