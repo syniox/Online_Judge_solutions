67fb55ca4cebfd0a29a032555659ce6d9be967ad
syniox <30317541+syniox@users.noreply.github.com>
2018-10-03 20:52:31 +0800

    [AC*] BZOJ3814

--- BZOJ/3814.cpp
+++ BZOJ/3814.cpp
@@ -8,12 +8,16 @@ const int N=1002,M=7;
 const int mod=1e9+7;
 int n,m,bar,dp[N][M][200],fdp[N][M][200];
 //0: none, 1: left, 2: right
-int cni,idx[N];
+int cni,idx[200],lk[200][8];
 bool mp[N][M];
 std::map <int,int> stt;
 
 template <class T> inline void twk(T &x){
 	if(x>=mod) x-=mod;
+}
+
+inline bool vld(const int k){
+	return !(idx[k]>>(m<<1)&3);
 }
 
 inline int nxi(){
@@ -69,7 +73,6 @@ inline void get_dp(int dp[N][M][200],const int step_i){
 						if(now==p1) --cnt;
 						if(now&&now!=p1&&++cnt>=0) break;
 					}
-					assert(l!=end);
 					const int to=cur^(p1<<(j<<1))^(p2<<((j+1)<<1))^(3<<(l<<1));
 					twk(dp[i][j+1][get_stt(to)]+=base);
 				}
@@ -82,35 +85,76 @@ inline void get_dp(int dp[N][M][200],const int step_i){
 	}
 }
 
+inline void prt_bin(const int x){
+	if(x==1) printf("01 ");
+	if(x==2) printf("10 ");
+	if(x==0) printf("00 ");
+}
+
+void prt_st(const int st){
+	for(int i=0;i<=m;++i){
+		prt_bin((st>>(i<<1))&3);
+	}
+	puts("");
+}
+
+inline void get_lk(){
+	memset(lk,-1,sizeof(lk));
+	static int stk[8];
+	for(int i=1;i<=cni;++i){
+		const int cur=idx[i];
+		if(cur>>(m<<1)&3) continue;
+		int tp,top=0;
+		for(int j=0;j<=m;++j){
+			if((tp=(cur>>(j<<1)&3))){
+				if(tp==1) stk[++top]=j;
+				else{
+					lk[i][j]=stk[top];
+					lk[i][stk[top--]]=j;
+				}
+			}
+		}
+	}
+}
+
+
+inline int jdg(const int x,const int y){
+	static bool vis[8];
+	memset(vis,0,sizeof(vis));
+	int st=0;
+	for(;lk[x][st]==-1;++st);
+	while(~st&&!vis[st]){
+		vis[st]=1;
+		st=lk[y][st];
+		if(st==-1) return 0;
+		vis[st]=1;
+		st=lk[x][st];
+	}
+	for(int i=0;i<=m;++i){
+		if(!vis[i]){
+			if(~lk[x][i]||~lk[y][i]) return 0;
+		}
+	}
+	return 1;
+}
+
 inline int get_ans(const int x,const int y){
 	int ans=0;
 	for(int i=1;i<=cni;++i){
-		if(!dp[x][m][i]) continue;
-		const int cur=idx[i];
-		if(!(cur>>((y-1)<<1)&3)||(cur>>(m<<1))) continue;
-		int lastr=0,tgt=0;
-		bool exist=0;
-		for(int j=0;j<m;++j){
-			if(((cur>>(j<<1))&3)==2) lastr=j;
-		}
-		for(int j=0;j<m;++j){
-			if(((cur>>(j<<1))&3)==1){
-				if(!exist){
-					tgt|=1<<(j<<1);
-				}
-			}
-			if(((cur>>(j<<1))&3)==2){
-				tgt|=(1+(j==lastr))<<(j<<1);
+		if(!dp[x][m][i]||!vld(i)||!(idx[i]>>((y-1)<<1)&3)) continue;
+		for(int j=1;j<=cni;++j){
+			if(!fdp[x+1][m][j]||!vld(j)||!(idx[i]>>((y-1)<<1)&3)) continue;
+			if(jdg(i,j)){
+				ans=(ans+(lint)dp[x][m][i]*fdp[x+1][m][j])%mod;
 			}
 		}
-		ans=(ans+dp[x][m][i]*(lint)fdp[x+1][m][get_stt(tgt)])%mod;
 	}
 	return ans;
 }
 
 int main(){
 #ifndef ONLINE_JUDGE
-//	freopen("a.in","r",stdin);
+	freopen("a.in","r",stdin);
 #endif
 	n=nxi(),m=nxi(),bar=nxi();
 	for(int i=1;i<=bar;++i){
@@ -119,6 +163,7 @@ int main(){
 	}
 	get_dp(dp,1);
 	get_dp(fdp,-1);
+	get_lk();
 	int q=nxi();
 	while(q--){
 		const int x=nxi(),y=nxi();
