fc19241f6858eb91cad5b8e3b2c80b976c04197a
syniox <30317541+syniox@users.noreply.github.com>
2018-10-25 21:29:01 +0800

    [AC*] AGC001D 构造

---
+++ AtCoder/AGC001D.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+const int N=1e5+2;
+int hx[N];
+
+inline int nxi(){
+	int x=0;
+	char c;
+	while((c=getchar())>'9'||c<'0');
+	while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
+	return x;
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+	freopen("b.in","r",stdin);
+#endif
+	int n=nxi(),m=nxi();
+	for(int i=1;i<=m;++i){
+		hx[i]=nxi();
+	}
+	for(int i=2;i<=m;++i){
+		if(hx[i]&1){
+			if(hx[1]&1){
+				if(hx[m]&1){
+					puts("Impossible");
+					return 0;
+				}
+				std::swap(hx[i],hx[m]);
+			}
+			else std::swap(hx[1],hx[i]);
+		}
+	}
+	for(int i=1;i<=m;++i){
+		printf("%d ",hx[i]);
+	}
+	puts("");
+	if(m==1&&n!=1){
+		printf("2\n%d 1\n",n-1);
+		return 0;
+	}
+	++hx[1];
+	if(!--hx[m]) --m;
+	printf("%d\n",m);
+	for(int i=1;i<=m;++i){
+		printf("%d ",hx[i]);
+	}
+	puts("");
+	return 0;
+}
