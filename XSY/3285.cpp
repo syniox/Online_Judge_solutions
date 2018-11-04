745a400d7bc2ad61604c2b385e4645a190ab5209
syniox <30317541+syniox@users.noreply.github.com>
2018-11-04 13:23:08 +0800

    [AC*] XSY3285

--- XSY/3285.cpp
+++ XSY/3285.cpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 #include <cstring>
 typedef long long lint;
-int n,cnt,hx[30];
-lint mx,limit,buk[(int)2e7];
+const int W=1.5e7+5;
+int n,cnt1,cnt2,hx[30];
+lint mx,limit,buk1[W],buk2[W];
 
 template <class T> inline void apx(T &x,T y){
 	if(x<y) x=y;
@@ -18,11 +19,12 @@ template <class T> inline T nxi(){
 	return x;
 }
 
-inline void build(const int x){
-	int tp=cnt;
-	for(int i=1;i<=tp;++i){
-		lint tp=buk[i];
-		for(tp*=x;tp<=limit;tp*=x) apx(mx,buk[++cnt]=tp);
+inline void build(lint buk[],int &cnt,const int x){
+	lint ddl=limit/x;
+	for(int i=1;i<=cnt;i++){
+		if(buk[i]<=ddl){
+			apx(mx,buk[++cnt]=buk[i]*x);
+		}
 	}
 }
 
@@ -31,14 +33,23 @@ int main(){
 //	freopen("b.in","r",stdin);
 #endif
 	n=nxi<int>(),limit=nxi<lint>();
-	mx=buk[++cnt]=1;
+	mx=buk1[++cnt1]=buk2[++cnt2]=1;
 	for(int i=1;i<=n;++i){
 		hx[i]=nxi<int>();
 	}
 	std::sort(hx+1,hx+n+1);
 	for(int i=n;i;--i){
-		build(hx[i]);
+		if(i&1) build(buk1,cnt1,hx[i]);
+		else build(buk2,cnt2,hx[i]);
 	}
-	printf("%lld\n%d\n",mx,cnt);
+	lint ans=0;
+	std::sort(buk1+1,buk1+cnt1+1);
+	std::sort(buk2+1,buk2+cnt2+1);
+	for(int i=1,j=cnt2;i<=cnt1;i++){
+		for(lint tp=limit/buk1[i];buk2[j]>tp;--j);
+		ans+=j;
+		apx(mx,buk1[i]*buk2[j]);
+	}
+	printf("%lld\n%lld\n",mx,ans);
 	return 0;
 }
