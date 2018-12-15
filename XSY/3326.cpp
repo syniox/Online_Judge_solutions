5e45c31665e0b22e4350a7e06758a92c747ec1d2
syniox <30317541+syniox@users.noreply.github.com>
2018-12-15 14:56:53 +0800

    [AC*] XSY3326 倍增

--- XSY/3326.cpp
+++ XSY/3326.cpp
@@ -9,11 +9,21 @@ template <class T> inline T san(T x){
 	return x;
 }
 
+inline char get_c(){
+	static char *h,*t,buf[50000];
+	if(h==t){
+		t=(h=buf)+fread(buf,1,20000,stdin);
+		if(h==t) return EOF;
+	}
+	return *h++;
+}
+
+
 inline int nxi(){
 	int x=0;
 	char c;
-	while((c=getchar())>'9'||c<'0');
-	while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
+	while((c=get_c())>'9'||c<'0');
+	while(x=x*10-48+c,(c=get_c())>='0'&&c<='9');
 	return x;
 }
 
@@ -44,10 +54,12 @@ inline void make_nx(){
 inline void backport(const int x,int y,const int val){
 	int l=x+n,r=x+n;
 	for(; y>1&&l<=r&&r-l<n; --y){
-		if(get_nx(l,y-1)<l) ++l;
-		else if(get_nx(l-1,y-1)>=l) --l;
-		if(get_nx(r,y-1)>r) --r;
-		else if(get_nx(r+1,y-1)<=r) ++r;
+		if(get_nx(l,y)>r) return;
+		if(get_nx(l,y)<l) ++l;
+		else if(get_nx(l-1,y)>=l) --l;
+		if(get_nx(r,y)<l) return;
+		if(get_nx(r,y)>r) --r;
+		else if(get_nx(r+1,y)<=r) ++r;
 	}
 	for(int i=l; i<=r; ++i){
 		nx[0][san(i)]=val;
@@ -57,13 +69,13 @@ inline void backport(const int x,int y,const int val){
 inline void change(){
 	int x=nxi(),y=nxi(),val=nxi();
 	int cur_y=y==1?m:y-1;
-	int bef[3],fx[3]={x==1?n:x-1,x,x==n?1:x+1};
+	int bef[3],fx[3]={san(x+n-1),x,san(x+1)};
 	for(int i=0; i<3; ++i){
-		bef[i]=get_to_1(fx[i],cur_y);
+		bef[i]=get_to_1(get_nx(fx[i],y),y);
 	}
 	mp[x][y]=val;
 	for(int i=0; i<3; ++i){
-		int tp=get_to_1(fx[i],cur_y);
+		int tp=get_to_1(get_nx(fx[i],y),y);
 		if(tp!=bef[i]){
 			backport(fx[i],cur_y,tp);
 		}
@@ -79,9 +91,9 @@ inline void move(int &cur_x,int &cur_y){
 	if(!step--) return; 
 	cur_y=1,cur_x=get_nx(cur_x,1);
 	for(int tp=step/m,i=29; i>=0; --i){
-		if(tp&(1<<i)) cur_x=nx[cur_x][i];
+		if(tp>>i&1) cur_x=nx[i][cur_x];
 	}
-	for(; step; --step,++cur_y){
+	for(step%=m; step; --step,++cur_y){
 		cur_x=get_nx(cur_x,cur_y+1);
 	}
 }
@@ -94,12 +106,12 @@ int main(){
 		}
 	}
 	for(int i=1; i<=n; ++i){
-		nx[0][i]=get_to_1(i,1);
+		nx[0][i]=get_to_1(get_nx(i,2),2);
 	}
 	make_nx();
 	for(int cur_x=1,cur_y=1,q=nxi(); q; --q){
 		char c;
-		while((c=getchar())>'z'||c<'a');
+		while((c=get_c())>'z'||c<'a');
 		if(c=='c') change();
 		else{
 			move(cur_x,cur_y);
