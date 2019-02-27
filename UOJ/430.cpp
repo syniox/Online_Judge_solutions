67ff26e3d5b80ffd3b364387c9eb77294341548c
syniox <30317541+syniox@users.noreply.github.com>
2019-02-27 15:33:11 +0800

    [AC*] UOJ430 分块 & 斜率优化DP

--- UOJ/430.cpp
+++ UOJ/430.cpp
@@ -2,8 +2,8 @@
 #include <cstdio>
 #include <cstring>
 typedef long long lint;
-const int N=1e2+5;
-const int S=10;
+const int N=1e5+5;
+const int S=320;
 int n;
 int lf[N],tm[N],val[N];
 lint s_val[N],dp[N];
@@ -42,20 +42,24 @@ namespace B{
 
 	inline void build_blk(const int p){
 		pt[p]=0;
-		for(int mx=0,i=p*S-1; i>=(p-1)*S; --i){
-			for(int bef=idx[p][pt[p]]; pt[p]>1; --pt[p]){
-				int bbef=idx[p][pt[p]-1];
+		for(int i=p*S-1,nxt; i>=(p-1)*S; i=nxt){
+			int res=i;
+			for(int j=nxt=i; j>=(p-1)*S&&xtm[j]==xtm[i]; nxt=--j){
+				if(dp[j]<dp[res]) res=j;
+			}
+			for(; pt[p]>1; --pt[p]){
+				int bef=idx[p][pt[p]-1];
+				int bbef=idx[p][pt[p]-2];
 				const lint bef_x=-xtm[bef]+xtm[bbef];
 				const lint bef_y=dp[bef]-dp[bbef];
-				const lint cur_x=-xtm[i]+xtm[bbef];
-				const lint cur_y=dp[i]-dp[bbef];
+				const lint cur_x=-xtm[res]+xtm[bbef];
+				const lint cur_y=dp[res]-dp[bbef];
 				if(!st_left(bef_x,bef_y,cur_x,cur_y)) break;
 			}
-			if(pt[p]&&xtm[idx[p][pt[p]]]<=xtm[i]&&dp[idx[p][pt[p]]]<=dp[i]){
+			if(pt[p]&&xtm[idx[p][pt[p]-1]]<=xtm[res]&&dp[idx[p][pt[p]-1]]<=dp[res]){
 				continue;
 			}
-			apx(mx,val[i]);
-			idx[p][pt[p]++]=i;
+			idx[p][pt[p]++]=res;
 		}
 	}
 
@@ -80,8 +84,9 @@ namespace B{
 		upd_que[++upd_cnt]=bel[l];
 	}
 
-	inline void upd_blk(){
+	inline void upd_blk(int limit){
 		for(int i=1; i<=upd_cnt; ++i){
+			if(upd_que[i]>limit) continue;
 			if(upd_que[i]!=upd_que[i-1]){
 				build_blk(upd_que[i]);
 			}
@@ -89,27 +94,29 @@ namespace B{
 	}
 
 	inline lint ask_blk(const int p,const lint v){
-		int l=0,r=pt[p],m1,m2;
+		int l=0,r=pt[p]-1,m1,m2;
 		int d1,d2;
-		while(l!=r){
+		while(l<r-1){
 			m1=(l+l+r)/3,d1=idx[p][m1];
 			m2=(l+r+r)/3,d2=idx[p][m2];
 			if(dp[d1]+xtm[d1]*v>dp[d2]+xtm[d2]*v) l=m1+1;
 			else r=m2;
 		}
-		return dp[l]+(xtm[d1]+tag[p])*v;
+		d1=idx[p][l];
+		d2=idx[p][r];
+		return tag[p]*v+std::min(dp[d1]+xtm[d1]*v,dp[d2]+xtm[d2]*v);
 	}
 
 	inline lint ask(const int l,const int r,int mx,const lint v){
 		lint ans=2e18;
-		if(l-r<=S){
+		if(r-l<=S){
 			for(int i=r; i>=l; --i){
 				apn(ans,dp[i]+mx*v);
 				apx(mx,tm[i]);
 			}
 			return ans;
 		}
-		for(int i=l; bel[i]==bel[l]; --i){
+		for(int i=r; bel[i]==bel[r]; --i){
 			apn(ans,dp[i]+mx*v);
 			apx(mx,tm[i]);
 		}
@@ -121,28 +128,29 @@ namespace B{
 			apn(ans,dp[i]+mx*v);
 			apx(mx,tm[i]);
 		}
+		return ans;
 	}
 }
 
 inline void solve(){
 	static int d_stk[N];//decresing stack for tm[i]
-	int top;
+	int top=0;
 	for(int i=1; i<=n; ++i){
+		B::upd_cnt=0;
+		B::upd_xtm(i-1,i-1,tm[i]);
 		for(int t=d_stk[top]; top&&tm[t]<tm[i]; t=d_stk[--top]){
-			B::upd_xtm(d_stk[top-1]+1,d_stk[top],tm[i]-tm[t]);
+			B::upd_xtm(d_stk[top-1],d_stk[top]-1,tm[i]-tm[t]);
 		}
 		while(top&&tm[d_stk[top]]==tm[i]){
 			--top;
 		}
 		d_stk[++top]=i;
-		B::upd_blk();
+		B::upd_blk(B::bel[i]-1);
 		dp[i]=B::ask(lf[i],i-1,tm[i],s_val[i+1]);
 		if(i%S==S-1){
 			B::build_blk(B::bel[i]);
 		}
-		//printf("%lld ",dp[i]);
 	}
-	//puts("");
 }
 
 int main(){
@@ -151,8 +159,8 @@ int main(){
 	for(int i=1; i<=n; ++i){
 		lf[i]=nxi();
 		tm[i]=nxi();
+		apx(B::b_mx[B::bel[i]],tm[i]);
 		val[i]=nxi();
-		apx(B::b_mx[B::bel[i]],val[i]);
 	}
 	for(int i=n; i; --i){
 		s_val[i]=val[i]+s_val[i+1];
