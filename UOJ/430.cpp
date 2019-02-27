790e7ad6bbb3e256ffd5cc851dd41341a83e75d2
syniox <30317541+syniox@users.noreply.github.com>
2019-02-27 15:32:11 +0800

    [RE] UOJ430

--- UOJ/430.cpp
+++ UOJ/430.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <cassert>
 typedef long long lint;
 const int N=1e2+5;
 const int S=10;
-int n,lf[N],val[N],cos[N];
-lint dp[N],s_cos[N];
+int n;
+int lf[N],tm[N],val[N];
+lint s_val[N],dp[N];
 
 template <class T> inline void apx(T &x,const T y){
 	if(x<y) x=y;
 }
 template <class T> inline void apn(T &x,const T y){
 	if(x>y) x=y;
+}
+
+inline bool st_left(const lint x1,const lint y1,const lint x2,const lint y2){
+	//vec 1 on the right side of vec2
+	return (__int128)x1*y2-(__int128)y1*x2>=0;
 }
 
 inline int nxi(){
@@ -24,126 +29,135 @@ inline int nxi(){
 }
 
 namespace B{
-	int bel[N],xnx[N],b_mx[S];
-	int tl[S],hd[S];
-	lint n_dp[N];
-	class _vec{
-		public:
-			lint x,y;//x->xnx,y->dp
-			friend _vec operator - (const _vec &a,const _vec &b){
-				return (_vec){a.x-b.x,a.y-b.y};
-			}
-	}idx[S][S+5];
-	//xnx[i]: current max after i (not all are satisfied)
+	int tag[S],b_mx[S],pt[S],idx[S][S];
+	int bel[N],xtm[N];
+	int upd_cnt,upd_que[N];
 
 	inline void init(){
-		for(int i=0; i<=n; ++i){
+		bel[0]=1;
+		for(int i=1; i<=n; ++i){
 			bel[i]=i/S+1;
 		}
 	}
 
-	inline bool st_left(const _vec &a,const _vec &b){
-		return (__int128)a.x*b.y-(__int128)a.y*b.x>0;
-	}
-
-	inline void build_g(const int p){
-		for(int i=p*S-1,mx=0; i>=(p-1)*S; --i){
-			if(i==p*S-1) n_dp[i]=dp[i];
-			else n_dp[i]=std::min(dp[i],n_dp[i+1]);
-			xnx[i]=mx;
-			_vec cur=(_vec){-xnx[i],dp[i]};
-			for(_vec bef=idx[p][tl[p]]; tl[p]>1; bef=idx[p][--tl[p]]){
-				_vec bbef=idx[p][tl[p]-1];
-				if(st_left(cur-bef,bef-bbef)) break;
+	inline void build_blk(const int p){
+		pt[p]=0;
+		for(int mx=0,i=p*S-1; i>=(p-1)*S; --i){
+			for(int bef=idx[p][pt[p]]; pt[p]>1; --pt[p]){
+				int bbef=idx[p][pt[p]-1];
+				const lint bef_x=-xtm[bef]+xtm[bbef];
+				const lint bef_y=dp[bef]-dp[bbef];
+				const lint cur_x=-xtm[i]+xtm[bbef];
+				const lint cur_y=dp[i]-dp[bbef];
+				if(!st_left(bef_x,bef_y,cur_x,cur_y)) break;
 			}
-			apx(mx,val[i]);
-			b_mx[p]=mx;
-			if(tl[p]&&-idx[p][tl[p]].x<=xnx[i]&&idx[p][tl[p]].y<=dp[i]){
+			if(pt[p]&&xtm[idx[p][pt[p]]]<=xtm[i]&&dp[idx[p][pt[p]]]<=dp[i]){
 				continue;
 			}
-			idx[p][++tl[p]]=cur;
+			apx(mx,val[i]);
+			idx[p][pt[p]++]=i;
 		}
-		hd[p]=1;
 	}
 
-	inline void ap_max(const int p,const int v){
-		if(-idx[p][hd[p]].x>v) return;
-		while(hd[p]<tl[p]&&-idx[p][hd[p]].x<=v) ++hd[p];
-		int l=(p-1)*S,r=p*S-1,mid;
+	inline void upd_xtm(const int l,const int r,const int delta){
+		if(bel[r]==bel[l]){
+			for(int i=l; i<=r; ++i){
+				xtm[i]+=delta;
+			}
+		}
+		else{
+			for(int i=r; bel[i]==bel[r]; --i){
+				xtm[i]+=delta;
+			}
+			for(int i=bel[l]+1; i<bel[r]; ++i){
+				tag[i]+=delta;
+			}
+			for(int i=l; bel[i]==bel[l]; ++i){
+				xtm[i]+=delta;
+			}
+		}
+		upd_que[++upd_cnt]=bel[r];
+		upd_que[++upd_cnt]=bel[l];
+	}
+
+	inline void upd_blk(){
+		for(int i=1; i<=upd_cnt; ++i){
+			if(upd_que[i]!=upd_que[i-1]){
+				build_blk(upd_que[i]);
+			}
+		}
+	}
+
+	inline lint ask_blk(const int p,const lint v){
+		int l=0,r=pt[p],m1,m2;
+		int d1,d2;
 		while(l!=r){
-			mid=(l+r)>>1;
-			if(xnx[mid]<=v) r=mid;
-			else l=mid+1;
+			m1=(l+l+r)/3,d1=idx[p][m1];
+			m2=(l+r+r)/3,d2=idx[p][m2];
+			if(dp[d1]+xtm[d1]*v>dp[d2]+xtm[d2]*v) l=m1+1;
+			else r=m2;
 		}
-		idx[p][--hd[p]]=(_vec){-v,n_dp[l]};
+		return dp[l]+(xtm[d1]+tag[p])*v;
 	}
 
-	inline lint qry_blk(const int p,const lint v){
-		static int lst_pos[N];
-		apx(lst_pos[p],hd[p]);
-		int dec=lst_pos[p];
-		for(_vec tp1,tp2; dec<tl[p]; ++dec){
-			tp1=idx[p][dec];
-			tp2=idx[p][dec+1];
-			if(tp1.y-tp1.x*v<tp2.y-tp2.x*v) break;
+	inline lint ask(const int l,const int r,int mx,const lint v){
+		lint ans=2e18;
+		if(l-r<=S){
+			for(int i=r; i>=l; --i){
+				apn(ans,dp[i]+mx*v);
+				apx(mx,tm[i]);
+			}
+			return ans;
 		}
-		lst_pos[p]=dec;
-		return idx[p][dec].y-idx[p][dec].x*v;
+		for(int i=l; bel[i]==bel[l]; --i){
+			apn(ans,dp[i]+mx*v);
+			apx(mx,tm[i]);
+		}
+		for(int i=bel[r]-1; i>bel[l]; --i){
+			apn(ans,ask_blk(i,v));
+			apx(mx,b_mx[i]);
+		}
+		for(int i=bel[l]*S-1; i>=l; --i){
+			apn(ans,dp[i]+mx*v);
+			apx(mx,tm[i]);
+		}
 	}
+}
 
-	inline void solve(){
-		init();
-		for(int i=1; i<=n; ++i){
-
-			for(int j=1; j<bel[i]; ++j){
-				ap_max(j,val[i]);
-			}
-			dp[i]=2e18;
-			if(i-lf[i]<=S){
-				for(int mx=val[i],j=i-1; j>=lf[i]; --j){
-					apx(mx,val[j+1]);
-					apn(dp[i],dp[j]+(lint)mx*s_cos[i+1]);
-				}
-			}
-			else{
-				int mx=val[i];
-				for(int j=i-1; bel[j]==bel[i]; --j){
-					apx(mx,val[j+1]);
-					apn(dp[i],dp[j]+(lint)mx*s_cos[i+1]);
-				}
-				apx(mx,val[(bel[i]-1)*S]);
-				for(int j=bel[i]-1; j>bel[lf[i]]; --j){
-					apx(mx,b_mx[j]);
-					apn(dp[i],qry_blk(j,s_cos[i+1]));
-				}
-				for(int j=bel[lf[i]]*S; j>=lf[i]; --j){
-					apx(mx,val[j+1]);
-					apn(dp[i],dp[j]+(lint)mx*s_cos[i+1]);
-				}
-			}
-			printf("%lld ",dp[i]);
-			//fprintf(stderr,"%lld ",dp[i]);
-			if(i%S==S-1) build_g(bel[i]);
+inline void solve(){
+	static int d_stk[N];//decresing stack for tm[i]
+	int top;
+	for(int i=1; i<=n; ++i){
+		for(int t=d_stk[top]; top&&tm[t]<tm[i]; t=d_stk[--top]){
+			B::upd_xtm(d_stk[top-1]+1,d_stk[top],tm[i]-tm[t]);
 		}
-		//we don't need to build bel[n] at this case
-		/*
-		dp[n]=2e18;
-		for(int i=n-1; i>=0&&i>=lf[n]; --i){
-			apn(dp[n],dp[i]);
+		while(top&&tm[d_stk[top]]==tm[i]){
+			--top;
 		}
-		*/
+		d_stk[++top]=i;
+		B::upd_blk();
+		dp[i]=B::ask(lf[i],i-1,tm[i],s_val[i+1]);
+		if(i%S==S-1){
+			B::build_blk(B::bel[i]);
+		}
+		//printf("%lld ",dp[i]);
 	}
+	//puts("");
 }
 
 int main(){
 	n=nxi();
+	B::init();
 	for(int i=1; i<=n; ++i){
-		lf[i]=nxi(),val[i]=nxi(),cos[i]=nxi();
+		lf[i]=nxi();
+		tm[i]=nxi();
+		val[i]=nxi();
+		apx(B::b_mx[B::bel[i]],val[i]);
 	}
 	for(int i=n; i; --i){
-		s_cos[i]=s_cos[i+1]+cos[i];
+		s_val[i]=val[i]+s_val[i+1];
 	}
-	B::solve();
+	solve();
 	printf("%lld\n",dp[n]);
 	return 0;
 }
