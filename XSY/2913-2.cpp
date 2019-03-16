c279581af88f535e4679aff99129ab61f6bc1efc
syniox <30317541+syniox@users.noreply.github.com>
2019-03-16 14:16:50 +0800

    [AC*] XSY2913 LCT

--- XSY/2913-2.cpp
+++ XSY/2913-2.cpp
@@ -5,11 +5,20 @@
 const int N=1e5+5;
 int n,m;
 
+inline char get_c(){
+	static char buf[20000],*h,*t;
+	if(h==t){
+		t=(h=buf)+fread(buf,1,20000,stdin);
+		if(h==t) return EOF;
+	}
+	return *h++;
+}
+
 inline int nxi(){
 	int x=0;
 	char c;
-	while((c=getchar())>'9'||c<'0');
-	while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
+	while((c=get_c())>'9'||c<'0');
+	while(x=x*10-48+c,(c=get_c())>='0'&&c<='9');
 	return x;
 }
 
@@ -58,7 +67,7 @@ namespace T{//LCT节点只维护"轻"儿子信息，不维护自己
 	inline void psh(const int x){
 		const int ls=tr[x].c[0],rs=tr[x].c[1];
 		if(ls) tr[ls].col=tr[x].col;
-		if(rs) tr[ls].col=tr[x].col;
+		if(rs) tr[rs].col=tr[x].col;
 	}
 
 	inline void add_v(const int x,const int f,const int flag){
@@ -92,7 +101,7 @@ namespace T{//LCT节点只维护"轻"儿子信息，不维护自己
 		else tr[x].col=tr[f].col;
 		int &p=tr[x].c[k^1];
 		tr[x].fa=tr[f].fa;
-		tr[p].fa=f;
+		if(p) tr[p].fa=f;
 		tr[f].fa=x;
 		tr[f].c[k]=p;
 		p=f;
@@ -115,14 +124,16 @@ namespace T{//LCT节点只维护"轻"儿子信息，不维护自己
 		//side0: cut at the top
 		splay(x);
 		if(!tr[x].c[side]) return;
+		psh(x);
 		int k=tr[x].c[side];
 		if(side==0){
 			tr[k].fa=tr[x].fa;//sum does not change
-			while(tr[k].c[side^1]) k=tr[k].c[side^1];
+			while(tr[k].c[1]) k=tr[k].c[1];
 			splay(k);
 			tr[x].fa=k;
+			tr[x].c[side]=0;
+			upd(x);
 			add_v(x,k,1);
-			tr[x].c[side]=0,upd(x);
 			upd(k);
 		}
 		else{
@@ -131,52 +142,76 @@ namespace T{//LCT节点只维护"轻"儿子信息，不维护自己
 		}
 	}
 
-	inline void acs(int x,const int tgt){
-		assert(isrt(tgt)&&!tr[tgt].c[0]);
-		for(int y=0; G::dep[x]-tr[tr[x].c[0]].sz>=G::dep[tgt]; y=x,x=tr[x].fa){
+	inline void merge(int x){
+		for(int y=0; x; y=x,x=tr[x].fa){
+			splay(x);
+			if(y&&tr[x].col!=tr[y].col) break;
 			cut(x,1);
 			add_v(y,x,-1);
+			tr[x].c[1]=y;
+			upd(x);
+		}
+	}
+
+	inline void acs(int x,const int tgt){
+		static int org[N][3];
+		assert(isrt(tgt)&&!tr[tgt].c[0]);
+		for(int i=x; i; i=tr[i].fa){
+			splay(i);
+			if(G::dep[i]-tr[tr[i].c[0]].sz<G::dep[tgt]) break;
+			org[i][0]=tr[i].ask(0);
+			org[i][1]=tr[i].ask(1);
+			org[i][2]=tr[i].ask(2);
+		}
+		for(int y=0; G::dep[x]-tr[tr[x].c[0]].sz>=G::dep[tgt]; y=x,x=tr[x].fa){
+			cut(x,1);
+			tr[x].sum[0]-=org[y][0],tr[x].val[0]-=org[y][0];
+			tr[x].sum[1]-=org[y][1],tr[x].val[1]-=org[y][1];
+			tr[x].sum[2]-=org[y][2],tr[x].val[2]-=org[y][2];
 			tr[x].c[1]=y,upd(x);
 		}
 	}
 
+	inline void add_delta(const int x,const int f,int org[3]){
+		for(int i=0; i<3; ++i){
+			int delta=tr[x].ask(i)-org[i];
+			tr[f].val[i]+=delta;
+			tr[f].sum[i]+=delta;
+		}
+	}
+
 	inline void mod(const int x,const int f,const int v){
-		static int org_v[N][3];
+		static int org_f[3],org_ff[3];
 		cut(f,0);
-		for(int i=f,j=tr[i].fa; j; i=j,j=tr[j].fa){
-			splay(i);
-			org_v[i][0]=tr[i].ask(0);
-			org_v[i][1]=tr[i].ask(1);
-			org_v[i][2]=tr[i].ask(2);
+		const int ff=tr[f].fa;
+		if(ff){
+			merge(ff),splay(ff);
+			org_f[0]=tr[f].ask(0);
+			org_f[1]=tr[f].ask(1);
+			org_f[2]=tr[f].ask(2);
+		}
+		const int fff=tr[ff].fa;
+		if(fff){
+			merge(fff),splay(fff);
+			org_ff[0]=tr[ff].ask(0);
+			org_ff[1]=tr[ff].ask(1);
+			org_ff[2]=tr[ff].ask(2);
 		}
 		acs(x,f);
 		splay(f);
 		tr[f].col=v;
-		int top=f;
-		for(int i=f,j=tr[i].fa,delta; j; top=i,i=j,j=tr[j].fa){
-			assert(isrt(i));
-			for(int k=0; k<3; ++k){
-				delta=tr[i].ask(k)-org_v[i][k];
-				tr[j].val[k]+=delta;
-				tr[j].sum[k]+=delta;
-			}
-			if(tr[j].col!=tr[i].col) break;
+		if(ff){
+			assert(isrt(ff));
+			add_delta(f,ff,org_f);
 		}
-		while(tr[top].c[0]) top=tr[top].c[0];
-		splay(top);
-		acs(x,top);
+		if(fff){
+			assert(isrt(fff));
+			add_delta(ff,fff,org_ff);
+		}
 	}
 
 	inline int ask(int x){
-		splay(x);
-		for(int y=tr[x].fa; y; x=y,y=tr[y].fa){
-			splay(y);
-			if(tr[x].col!=tr[y].col) break;
-			cut(y,1);
-			tr[y].c[1]=x;
-			add_v(x,y,-1);
-			upd(y);
-		}
+		merge(x),splay(x);
 		return tr[x].ask(tr[x].col);
 	}
 }
