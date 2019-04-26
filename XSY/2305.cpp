bd79e8bea058f5287ba70facccaf5c3eb03bb93f
syniox <30317541+syniox@users.noreply.github.com>
2019-04-27 07:57:39 +0800

    [WA] XSY2305 凸包

--- XSY/2305.cpp
+++ XSY/2305.cpp
@@ -57,8 +57,11 @@ int andrew(vec *v,int &n){
 		que[++top]=v[i];
 	}
 	for(; top>turn_p&&crs(que[top]-que[top-1],que[1]-que[top-1])<=0; --top);
-	memcpy(v+1,que+1,top*sizeof(vec));
-	n=top;
+	n=0;
+	for(int i=1; i<=top; ++i){
+		if(i!=1&&que[i]==que[i-1]&&i<=turn_p) --turn_p;
+		else v[++n]=que[i];
+	}
 	return turn_p;
 }
 
@@ -72,51 +75,81 @@ bool bd_same(){
 	return 0;
 }
 
-bool prework(){
-	int turn_p=andrew(bd,n);
-	for(int i=1; i<=m; ++i){
-		{
-			int l=0,r=turn_p,mid;
-			//线段左端点
-			while(l!=r){
-				mid=(l+r+1)>>1;
-				if(bd[mid].x>pnt[i].x) r=mid-1;
-				else l=mid;
-			}
-			if(l==0||l==turn_p) continue;
-			if(crs(bd[l-1]-bd[l],pnt[i]-bd[l])<0) continue;
-		}
-		{
-			int l=turn_p-1,r=n,mid;
-			//线段右端点
-			while(l!=r){
-				mid=(l+r+1)>>1;
-				if(bd[mid].x<pnt[i].x) r=mid-1;
-				else l=mid;
-			}
-			if(l<turn_p||l==n) continue;
-			if(crs(bd[l-1]-bd[l],pnt[i]-bd[l])>=0) return 0;
-		}
+inline bool inside(const vec &x,const vec *v,const int len,const int turn_p){
+	if(x.x<v[1].x||x.x>v[turn_p].x) return 0;
+	if(v[1].x==x.x&&v[len].x==x.x){
+		return v[1].y<=x.y&&v[len].y>=x.y;
 	}
-	andrew(pnt,m);
+	if(turn_p>1&&v[turn_p].x==x.x&&v[turn_p-1].x==x.x){
+		return v[turn_p].y>=x.y&&v[turn_p-1].y<=x.y;
+	}
+	//考虑边界
+	{
+		int l=1,r=turn_p,mid;
+		//线段左端点
+		//最后一个横座标小于x的点(在边界处卡成等于)
+		while(l!=r){
+			mid=(l+r+1)>>1;
+			if(v[mid].x>=x.x) r=mid-1;
+			else l=mid;
+		}
+		assert(l!=turn_p);
+		if(crs(v[l+1]-v[l],x-v[l])<0) return 0;
+	}
+	{
+		int l=turn_p,r=len,mid;
+		//线段右端点
+		//最后一个横座标大于x的点(在边界处卡成等于)
+		while(l!=r){
+			mid=(l+r+1)>>1;
+			if(v[mid].x<=x.x) r=mid-1;
+			else l=mid;
+		}
+		if(crs(v[l==len?1:l+1]-v[l],x-v[l])<0) return 0;
+	}
 	return 1;
+}
+
+int prework(){
+	int turn_pt=andrew(pnt,m);
+	if(m==1) return -bd_same();
+	andrew(bd,n);
+	for(int i=1; i<=n; ++i){
+		if(!inside(bd[i],pnt,m,turn_pt)) return 0;
+	}
+	return 1;
+}
+
+inline bool need_go(const vec &pre,const vec &nxt,const vec &cur){
+	if(nxt==cur) return 0;
+	return crs(pre-cur,nxt-cur)<=0;
 }
 
 bool getjmp(){
 	for(int i=1,j=1,k=1; i<=m; ++i){
 		//j: 内凸包最近点
-		for(; j<n&&crs(bd[j+1]-pnt[i],bd[j]-pnt[i])>=0; ++j);
-		if(j==n&&crs(bd[1]-pnt[i],bd[j]-pnt[i])>=0){
-			for(j=1; j<n&&crs(bd[j+1]-pnt[i],bd[j]-pnt[i])>=0; ++j); 
+		for(; j<n&&need_go(bd[j],bd[j+1],pnt[i]); ++j);
+		if(j==n&&need_go(bd[n],bd[1],pnt[i])){
+			for(j=1; j<n&&need_go(bd[j],bd[j+1],pnt[i]); ++j);
 		}
 		for(; k<m&&crs(bd[j]-pnt[i],pnt[k+1]-pnt[i])<=0; ++k);
-		if(k==m&&crs(bd[j]-pnt[i],pnt[1]-pnt[i])<=0){
-			for(k=1; k!=i&&crs(bd[j]-pnt[i],pnt[k+1]-pnt[i])<=0; ++k);
-			if(k==i) return 0;
+		if(k==m&&i!=1&&crs(bd[j]-pnt[i],pnt[1]-pnt[i])<=0){
+			for(k=1; k+1<i&&crs(bd[j]-pnt[i],pnt[k+1]-pnt[i])<=0; ++k);
 		}
 		jmp[i]=k;
+		if(k==i) ++k;
 	}
 	return 1;
+}
+
+inline int getans(const int p){
+	int res=0,step=0;
+	for(int x=p; step<m; x=jmp[x]){
+		++res;
+		if(jmp[x]==x) return 1e9;
+		step+=(jmp[x]-x+m)%m;
+	}
+	return res;
 }
 
 int main(){
@@ -133,31 +166,20 @@ int main(){
 	}
 	for(int i=1; i<=m; ++i){
 		pnt[i].x=nxi(),pnt[i].y=nxi();
-		wpos.insert(pnt[i]);
-	}
-	for(int i=1,j=0; i<=n; ++i){
-		if(wpos.find(bd[i])==wpos.end()){
-			bd[++j]=bd[i];
-		}
-		if(i==n) n=j;
 	}
 	if(bd_same()){
 		puts("1");
 		return 0;
 	}
-	if(!prework()||!getjmp()){
-		puts("-1");
-		return 0;
+	switch(prework()){
+		case -1: puts("1"); return 0;
+		case 0: puts("-1"); return 0;
 	}
+	getjmp();
 	int ans=1e9;
 	for(int i=1; i<=m; ++i){
-		int res=0,step=0;
-		for(int x=i; step<m; x=jmp[x]){
-			++res;
-			step+=(jmp[x]-x+m)%m;
-		}
-		apn(ans,res);
+		apn(ans,getans(i));
 	}
-	printf("%d\n",ans);
+	printf("%d\n",ans==1e9?-1:ans);
 	return 0;
 }
