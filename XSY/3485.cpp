656ecbb6b1fa1f35d0b366a93801cb328aebd72a
syniox <30317541+syniox@users.noreply.github.com>
2019-06-14 21:07:28 +0800

    [AC*] XSY3485 后缀树 & 平衡树

--- XSY/3485.cpp
+++ XSY/3485.cpp
@@ -1,3 +1,5 @@
+//Treap index比SAM多1
+
 #include <iostream>
 #include <cstdio>
 #include <cstring>
@@ -16,7 +18,7 @@ struct _qry{
 }qry[N*5];
 struct _pair{
 	int x,y;
-}mov[N];
+}mov[N*5];
 
 namespace utils{
 	template <class T> inline void apx(T &x,const T y){
@@ -37,21 +39,26 @@ namespace utils{
 using namespace utils;
 
 inline bool cmp_y(const _pair &a,const _pair &b){
-	return a.y<b.y;
+	return a.y>b.y;
 }
 
 namespace T{
 	//小根堆
-	int rt,*idx;
+	int rt;
+	const int *idx;
+	const bool *vld;
 	struct node{
 		//fa只保证merge之后正确
-		int ls,rs,w,fa,sz;
-	}tr[N];
+		int ls,rs,w,fa,sz,sz_t;
+		bool vld;
+	}tr[N<<1];
 
 	inline void upd(const int k){
-		tr[k].sz=tr[tr[k].ls].sz+tr[tr[k].rs].sz+1;
-		if(tr[k].ls) tr[tr[k].ls].fa=k;
-		if(tr[k].rs) tr[tr[k].rs].fa=k;
+		const int ls=tr[k].ls,rs=tr[k].rs;
+		tr[k].sz=tr[ls].sz+tr[rs].sz+vld[k];
+		tr[k].sz_t=tr[ls].sz_t+tr[rs].sz_t+1;
+		if(ls) tr[ls].fa=k;
+		if(rs) tr[rs].fa=k;
 	}
 
 	int build_t(const int l,const int r){
@@ -59,19 +66,19 @@ namespace T{
 		for(int i=l+1; i<=r; ++i){
 			if(tr[idx[i]].w<tr[idx[rt]].w) rt=i;
 		}
-		tr[idx[rt]].sz=r-l+1;
 		if(l<rt) tr[idx[rt]].ls=build_t(l,rt-1);
 		if(r>rt) tr[idx[rt]].rs=build_t(rt+1,r);
 		upd(idx[rt]);
 		return idx[rt];
 	}
 
-	void build(int *idx){
+	void build(const int *idx,const bool *vld,const int len){
 		T::idx=idx;
-		for(int i=1; i<=n; ++i){
+		T::vld=vld;
+		for(int i=1; i<=len; ++i){
 			tr[i].w=rand();
 		}
-		rt=build_t(1,n);
+		rt=build_t(1,len);
 	}
 
 	void split(const int k,int &r1,int &r2,const int v){
@@ -79,7 +86,7 @@ namespace T{
 			r1=r2=0;
 			return;
 		}
-		const int sz_l=tr[tr[k].ls].sz;
+		const int sz_l=tr[tr[k].ls].sz_t;
 		if(sz_l>=v){
 			r2=k;
 			split(tr[k].ls,r1,tr[k].ls,v);
@@ -113,35 +120,35 @@ namespace T{
 	int kth(const int k,const int v){
 		assert(k&&v>0);
 		const int sz_l=tr[tr[k].ls].sz;
-		if(sz_l+1==v) return k;
 		if(sz_l>=v) return kth(tr[k].ls,v);
-		return kth(tr[k].rs,v-sz_l-1);
+		if(vld[k]&&sz_l+vld[k]==v) return k;
+		return kth(tr[k].rs,v-sz_l-vld[k]);
 	}
 
-	inline int rnk(int q){
-		int ans=tr[tr[q].ls].sz+1;;
+	inline int rnk_t(int q){
+		int ans=tr[tr[q].ls].sz_t+1;;
 		for(int f=tr[q].fa; f; q=f,f=tr[q].fa){
-			if(tr[f].rs==q) ans+=tr[tr[f].ls].sz+1;
+			if(tr[f].rs==q) ans+=tr[tr[f].ls].sz_t+1;
 		}
 		return ans;
 	}
 
 	void shift(const int l,const int r,const int p){
-		//[l,r]移到原来的p之前(r<p)
-		assert(r<p);
-		if(r==p-1) return;
-		int r1,r2,r3,r4;
-		split(rt,r3,r4,p-1);
-		split(r3,r2,r3,r);
-		split(r2,r1,r2,l-1);
-		merge(r2,r3,r2);
-		merge(r1,r1,r2);
-		merge(rt,r1,r4);
+		//[l,r]移到原来的p之后(l>p)
+		assert(l>p);
+		if(l==p+1) return;
+		int r1,r2,r3;
+		split(rt,rt,r3,r);
+		split(rt,rt,r2,l-1);
+		split(rt,rt,r1,p);
+		merge(rt,rt,r2);
+		merge(rt,rt,r1);
+		merge(rt,rt,r3);
 	}
 }
 
 namespace S{
-	int cnt,lst,id[N],fr_c[N],idfn[N],cnd;
+	int cnt,lst,id[N],idfn[N<<1],cnd;
 	bool vld[N<<1];
 	std::map <int,int> nxt[N<<1];
 	struct node{
@@ -167,7 +174,6 @@ namespace S{
 		else{
 			tr[++cnt]=tr[q];
 			tr[cnt].len=tr[p].len+1;
-			tr[cnt].pos=tr[p].pos-1;
 			tr[cnt].sz=0;
 			for(; ~p&&tr[p].c[c]==q; p=tr[p].lk){
 				tr[p].c[c]=cnt;
@@ -177,9 +183,8 @@ namespace S{
 	}
 
 	void dfs_sz(const int x){
-		if(tr[x].sz){
-			idfn[++cnd]=tr[x].pos;
-		}
+		idfn[++cnd]=x+1;
+		tr[x].sz=1;
 		for(std::map <int,int> ::iterator it=nxt[x].begin(); it!=nxt[x].end(); ++it){
 			dfs_sz(it->second);
 			tr[x].sz+=tr[it->second].sz;
@@ -187,16 +192,15 @@ namespace S{
 	}
 
 	void build(){
-		for(int i=1; i<=n; ++i){
-			fr_c[i]=tr[id[i]].pos+tr[tr[id[i]].lk].len;
-		}
 		for(int i=1; i<=cnt; ++i){
 			const int f=tr[i].lk,p=tr[i].pos+tr[f].len;
+			mov[i]=(_pair){i,str[p]};
 			assert(nxt[f].find(str[p])==nxt[f].end());
 			nxt[f][str[p]]=i;
 		}
+		std::sort(mov+1,mov+cnt+1,cmp_y);
 		dfs_sz(0);
-		T::build(idfn);
+		T::build(idfn,vld-1,cnt+1);
 	}
 }
 
@@ -216,17 +220,13 @@ int main(){
 		S::insert(str[i],i);
 	}
 	S::build();
-	for(int i=1; i<=n; ++i){
-		mov[i]=(_pair){i,S::fr_c[i]};
-	}
-	std::sort(mov+1,mov+n+1,cmp_y);
 	for(int i=1,j=1; i<=q; ++i){
-		for(; j<=n&&mov[j].y+qry[i].dlt>=m; ++j){
-			const int x=mov[j].x,sf=S::tr[S::id[x]].lk;
-			int p=T::rnk(x),sz=S::tr[S::id[x]].sz;
-			T::shift(p,p+sz-1,p-S::vld[sf]+S::tr[sf].sz);
+		for(; j<=S::cnt&&mov[j].y+qry[i].dlt>=m; ++j){
+			const int x=mov[j].x,sf=S::tr[x].lk;
+			int p=T::rnk_t(x+1),sz=S::tr[x].sz;
+			T::shift(p,p+sz-1,p+sz-S::tr[sf].sz);
 		}
-		ans[qry[i].id]=T::kth(T::rt,qry[i].kth);
+		ans[qry[i].id]=S::tr[T::kth(T::rt,qry[i].kth)-1].pos;
 	}
 	for(int i=1; i<=q; ++i){
 		printf("%d\n",ans[i]);
