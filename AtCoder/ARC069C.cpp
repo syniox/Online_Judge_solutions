c16eb53055ba10ce0677653e7e6470ce889c7445
syniox <30317541+syniox@users.noreply.github.com>
2019-07-05 19:23:51 +0800

    [AC] ARC069C

---
+++ AtCoder/ARC069C.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+typedef long long lint;
+
+namespace utils{
+	template <class T> inline void apn(T &x,const T y){x>y?x=y:0;}
+	template <class T> inline void apx(T &x,const T y){x<y?x=y:0;}
+	inline lint nxi(){
+		lint x=0;
+		char c;
+		while(((c=getchar())>'9'||c<'0')&&c!='-');
+		const bool f=(c=='-')&&(c=getchar());
+		while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
+		return f?-x:x;
+	}
+}
+using namespace utils;
+
+int main(){
+	lint x=nxi(),y=nxi();
+	lint lim=std::min(x,y>>1);
+	y-=lim*2;
+	printf("%lld\n",lim+(y>>2));
+}
