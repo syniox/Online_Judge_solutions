62bdb243f6ac7a47a08fb18f3ffee8cb446002d9
syniox <30317541+syniox@users.noreply.github.com>
2019-07-10 21:20:27 +0800

    [AC*] ARC072D 博弈(性质)

---
+++ AtCoder/ARC072D.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+typedef long long lint;
+
+namespace utils{
+	template <class T> inline void apn(T &x,const T y){x>y?x=y:0;}
+	template <class T> inline void apx(T &x,const T y){x<y?x=y:0;}
+	inline int nxi(){
+		int x=0;
+		char c;
+		while(((c=getchar())>'9'||c<'0')&&c!='-');
+		const bool f=(c=='-')&&(c=getchar());
+		while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
+		return f?-x:x;
+	}
+}
+using namespace utils;
+
+int main(){
+	lint a=nxi(),b=nxi();
+	puts(a-b<2&&b-a<2?"Brown":"Alice");
+	return 0;
+}
