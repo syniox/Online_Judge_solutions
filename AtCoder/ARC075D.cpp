f36665b8fa5817c71d0dd68c11465e69a82bd11f
syniox <30317541+syniox@users.noreply.github.com>
2019-07-12 20:36:49 +0800

    [AC] ARC075D 二分

--- AtCoder/ARC075D.cpp
+++ AtCoder/ARC075D.cpp
@@ -1,48 +1,48 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cmath>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
 typedef long long lint;
-lint fc[18];
+const int N=1e5+5;
+int n,qa,qb,val[N];
 
-inline int nxi(){
-	int x=0;
-	char c;
-	while((c=getchar())>'9'||c<'0');
-	while(x=x*10+c-48,(c=getchar())>='0'&&c<='9');
-	return x;
+namespace utils{
+	template <class T> inline void apn(T &x,const T y){x>y?x=y:0;}
+	template <class T> inline void apx(T &x,const T y){x<y?x=y:0;}
+	inline int nxi(){
+		int x=0;
+		char c;
+		while(((c=getchar())>'9'||c<'0')&&c!='-');
+		const bool f=(c=='-')&&(c=getchar());
+		while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
+		return f?-x:x;
+	}
 }
+using namespace utils;
 
-int dfs(int t,int lt,lint tp){
-	if(t>lt>>1) return tp==0;
-	lint cur=fc[lt-t]-fc[t-1],p=tp/cur,ans=0;
-	if(p*cur>tp) --p;
-	bool f=(t!=1);
-	if(abs(p)<=8+f) ans+=dfs(t+1,lt,tp-p*cur)*(9-abs(p)+f);
-	if(abs(p+1)<=8+f) ans+=dfs(t+1,lt,tp-(p+1)*cur)*(9-abs(p+1)+f);
-	return ans;
+bool jdg(const int t){
+	lint used=0;
+	for(int i=1; used<=t&&i<=n; ++i){
+		if(val[i]>(lint)t*qb){
+			used+=(val[i]-t*qb+qa-qb-1)/(qa-qb);
+		}
+	}
+	return used<=t;
 }
 
 int main(){
-#ifndef ONLINE_JUDGE
-//	freopen("a.in","r",stdin);
-#endif
-	fc[0]=1;
-	for(int i=1;i<18;++i) fc[i]=fc[i-1]*10;
-	lint d=nxi(),ans=0;
-	for(int i=1;i<=18;++i){
-/*		int cnt=1,tp=d;
-		if(i&1) cnt*=9;
-		for(int j=1;j<=i>>1;++j){
-			lint cur=fc[i-j]-fc[j-1],p=tp/cur;
-			if(p>8+(bool)(j!=1)) break;
-			cnt*=(9-p+(bool)(j!=1));
-			tp-=p*cur;
-		}
-		if(!tp) ans+=cnt;*/
-		int t=i==1?9:10;
-		ans+=dfs(1,i,d)*(i&1?t:1);
+	n=nxi(),qa=nxi(),qb=nxi();
+	for(int i=1; i<=n; ++i){
+		val[i]=nxi();
 	}
-	printf("%lld\n",ans);
+	{
+		int l=1,r=1e9,mid;
+		while(l!=r){
+			mid=(l+r)>>1;
+			if(jdg(mid)) r=mid;
+			else l=mid+1;
+		}
+		printf("%d\n",l);
+	}
 	return 0;
 }
