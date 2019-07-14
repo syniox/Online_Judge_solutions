21f9ecf0621daba64b54ce7b18f3e1f13bfaf76f
syniox <30317541+syniox@users.noreply.github.com>
2019-07-14 09:25:57 +0800

    [AC] ARC077C

---
+++ AtCoder/ARC077C.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+typedef long long lint;
+const int N=1e5+5;
+int n,val[N];
+
+namespace utils{
+	template <class T> inline void apn(T &x,const T y){x>y?x=y:0;}
+	template <class T> inline void apx(T &x,const T y){x<y?x=y:0;}
+	inline int nxi(){
+		int x=0;
+		char c;
+		while(((c=getchar())>'9'||c<'0')&&c!='-');
+		const bool f=(c=='-')&&(c=getchar());
+		while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
+		return f?-x:x;
+	}
+}
+using namespace utils;
+
+int main(){
+	n=nxi();
+	for(int i=1; i<=n; ++i){
+		val[i]=nxi();
+	}
+	for(int i=n; i>=1; i-=2){
+		printf("%d ",val[i]);
+	}
+	for(int i=n&1?2:1; i<=n; i+=2){
+		printf("%d ",val[i]);
+	}
+	puts("");
+	return 0;
+}
