7e4cd3b435d929edc88fcf1b989f1f24cbc26e7e
syniox <30317541+syniox@users.noreply.github.com>
2019-07-14 10:54:11 +0800

    [AC] ARC078C

---
+++ AtCoder/ARC078C.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+typedef long long lint;
+const int N=2e5+5;
+int n,val[N];
+lint sum;
+
+namespace utils{
+	template <class T> inline void apn(T &x,const T y){x>y?x=y:0;}
+	template <class T> inline void apx(T &x,const T y){x<y?x=y:0;}
+	template <class T> inline T cabs(const T &x){return x<0?-x:x;}
+	inline int nxi(){
+		int x=0;
+		char c;
+		while(((c=getchar())>'9'||c<'0')&&c!='-');
+		const bool f=(c=='-')&&(c=getchar());
+		while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
+		return f?-x:x;
+	}
+}
+using namespace utils;
+
+int main(){
+	n=nxi();
+	for(int i=1; i<=n; ++i){
+		val[i]=nxi();
+		sum+=val[i];
+	}
+	lint ans=2e15,tmp=0;
+	for(int i=1; i<n; ++i){
+		tmp+=val[i];
+		apn(ans,cabs(tmp-(sum-tmp)));
+	}
+	printf("%lld\n",ans);
+	return 0;
+}
