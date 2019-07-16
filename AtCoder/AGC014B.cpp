b3c3f26a65fcf96117660e2c9fd7d066622a7109
syniox <30317541+syniox@users.noreply.github.com>
2019-07-16 21:12:36 +0800

    [AC] AGC014B

---
+++ AtCoder/AGC014B.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+typedef long long lint;
+const int N=1e5+5;
+int n,dgr[N];
+
+namespace utils{
+	template <class T> inline void apn(T &x,const T y){x=x<y?x:y;}
+	template <class T> inline void apx(T &x,const T y){x=x<y?y:x;}
+	inline int nxi(){
+		int x=0;
+		char c;
+		while(((c=getchar())>'9'||c<'0')&&c!='-');
+		const bool f=(c=='-')&&(c=getchar());
+		while(x=x*10-48+c,(c=getchar())>='0'&&c<='9');
+		return f?-x:x;
+	}
+}
+using namespace utils;
+
+int main(){
+	n=nxi();
+	for(int i=nxi(); i; --i){
+		++dgr[nxi()];
+		++dgr[nxi()];
+	}
+	for(int i=1; i<=n; ++i){
+		if(dgr[i]&1){
+			puts("NO");
+			return 0;
+		}
+	}
+	puts("YES");
+	return 0;
+}
